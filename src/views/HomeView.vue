a9603c9a67207e46877e8acc63234d782e3192cf
nrisa <risanussy07@gmail.com>
2023-11-29 21:54:57 +0700

    up fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,7 @@
 <script setup>
-import { computed, ref, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useMainStore } from '@/stores/main'
-import {
-  mdiAccountMultiple,
-  mdiCartOutline,
-  mdiChartTimelineVariant,
-  mdiMonitorCellphone,
-  mdiReload,
-  mdiGithub,
-  mdiChartPie
-} from '@mdi/js'
 import * as chartConfig from '@/components/Charts/chart.config.js'
-import LineChart from '@/components/Charts/LineChart.vue'
-import SectionMain from '@/components/SectionMain.vue'
-import CardBoxWidget from '@/components/CardBoxWidget.vue'
-import CardBox from '@/components/CardBox.vue'
-import TableSampleClients from '@/components/TableSampleClients.vue'
-import NotificationBar from '@/components/NotificationBar.vue'
-import BaseButton from '@/components/BaseButton.vue'
-import CardBoxTransaction from '@/components/CardBoxTransaction.vue'
-import CardBoxClient from '@/components/CardBoxClient.vue'
-import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
-import SectionTitleLineWithButton from '@/components/SectionTitleLineWithButton.vue'
-import SectionBannerStarOnGitHub from '@/components/SectionBannerStarOnGitHub.vue'
 
 const chartData = ref(null)
 
@@ -31,15 +10,18 @@ const fillChartData = () => {
 }
 
 onMounted(() => {
+  // Execute code when the component is mounted
+  
   fillChartData()
 })
 
 const mainStore = useMainStore()
 
-const clientBarItems = computed(() => mainStore.clients.slice(0, 4))
+const clientBarItems = ref(mainStore.clients.slice(0, 4))
 
-const transactionBarItems = computed(() => mainStore.history)
+const transactionBarItems = ref(mainStore.history)
 </script>
+
 
 <template>
   <LayoutAuthenticated>
